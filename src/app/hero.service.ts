6bf17460fdcc7c5716a0afc26e71422ce699b661
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-02-28 14:44:43 +0200

    Update the HeroService getHero()

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -25,14 +25,17 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
+        tap(_ => this.log('fetched heroes')),
         catchError(this.handleError<Hero[]>('getHeroes', []))
       );
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
