a2d8faeb3d2e0a59c9665cb750d1be57fbb87731
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-02-28 14:57:53 +0200

    add posibility to update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
@@ -44,5 +48,13 @@ export class HeroService {
       this.log(`${operation} failed: ${error.message}`);
       return of(result as T);
     };
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 }
