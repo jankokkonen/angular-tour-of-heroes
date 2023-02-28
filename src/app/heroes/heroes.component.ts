23297829d2ac14e5f8407a063bde1b1871fc541a
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-02-28 12:18:10 +0200

    Modify routes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,30 +8,17 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
-  selectedHero?: Hero;
-
+    heroes: Hero[] = [];
   
-
-  //Add a private heroService parameter of type HeroService to the constructor:
-  constructor(private heroService: HeroService,
-    private messageService: MessageService) 
-    {}
-
-  ngOnInit(): void {
-    this.getHeroes();
+    constructor(private heroService: HeroService) { }
+  
+    ngOnInit(): void {
+      this.getHeroes();
+    }
+  
+    getHeroes(): void {
+      this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+    }
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add
-    (`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-  // Create a method to retrieve the heroes from the service:
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes)
-  }
-}
