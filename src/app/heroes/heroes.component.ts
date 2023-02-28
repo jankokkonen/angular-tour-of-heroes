8df47d19f7205afb0bbb8230429630fd4618772a
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-02-28 13:11:54 +0200

    add Routable HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,17 +8,30 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-    heroes: Hero[] = [];
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
   
-    constructor(private heroService: HeroService) { }
-  
-    ngOnInit(): void {
-      this.getHeroes();
-    }
-  
-    getHeroes(): void {
-      this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-    }
+
+  //Add a private heroService parameter of type HeroService to the constructor:
+  constructor(private heroService: HeroService,
+    private messageService: MessageService) 
+    {}
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add
+    (`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  // Create a method to retrieve the heroes from the service:
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+}
